fee36c7e726b8259d5e6b7900e0bdc711bd431aa
mscottcam <mscottcam@gmail.com>
2017-07-07 10:21:48 -0500

    initial setup

---
+++ config.js
@@ -0,0 +1,10 @@
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://localhost/hacker-news-api';
+
+exports.DATABASE = {
+  client: 'pg',
+  connection: DATABASE_URL,
+  pool: { min: 0, max: 3 },
+  debug: true
+};
+
+exports.PORT = process.env.PORT || 8080; 
